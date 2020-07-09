af8d6ead4a25b1b5027ac1a851fe726a2e2b89d9
Stefan Eiermann <github@ultraapp.de>
2020-07-09 03:20:09 +0200

    Create daily.php

---
+++ daily.php
@@ -0,0 +1,12 @@
+<?php
+
+  include "init.php";
+  
+  include "count_clients.php";
+  include "count_domains.php";
+  include "count_mailboxes.php";
+  include "monthly_visits.php";
+  include "traffic_domain_month.php";
+
+
+?>
