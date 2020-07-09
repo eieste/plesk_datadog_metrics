6bf3219a1b9d31556dce2fd080bc647febec33f5
Stefan Eiermann <github@ultraapp.de>
2020-07-09 03:07:28 +0200

    Update init.php

--- init.php
+++ init.php
@@ -4,6 +4,7 @@
  * Common boostrap routines from cron jobs
  *
  * @author Israel Viana isra00@gmail.com
+ * @editor Stefan Eiermann github@ultraapp.de
  * @license http://www.gnu.org/copyleft/gpl.html
  */
 
@@ -11,14 +12,14 @@ include_once 'vendor/datadogstatsd.php';
 
 define('DATADOG_APIKEY', 'your datadog api key');
 define('DATADOG_APPKEY', 'your datadog app key');
+define('DATADOG_SITE', 'https://app.datadoghq.eu');
 
 define('MYSQL_HOST', 'localhost');
 define('MYSQL_USER', 'admin');
 define('MYSQL_PASSWORD', 'your admin pasword');
 define('MYSQL_DB', 'psa');
 
-mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD);
-mysql_select_db(MYSQL_DB);
+$conn = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB);
 
-DataDogStatsD::configure(DATADOG_APIKEY, DATADOG_APPKEY);
+DataDogStatsD::configure(DATADOG_APIKEY, DATADOG_APPKEY, DATADOG_SITE);
 
